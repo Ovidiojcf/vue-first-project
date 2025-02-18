f15ee1a9f8b072d252aee7f4c9636d190d71b55c
Ovidiojcf <ojcfarias@gmail.com>
2025-02-18 16:33:46 -0300

    update firstComponent

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # vue-first-project
 
-This template should help get you started developing with Vue 3 in Vite.
+First classes about Vue with materials From youtube and Udemy.
 
 ## Recommended IDE Setup
 
--- src/components/firstComponent.vue
+++ src/components/firstComponent.vue
@@ -1,9 +1,19 @@
 <template>
     <h1>Hello Ovídio! How is it going?</h1>
+    <div>
+        <h1>If you're upset {{ name }}, don't worry. Everything it's gonna be okay!!</h1>
+        <h1>You will become a {{ profession }}</h1>
+    </div>
 </template>
 <!--Após a tag tamplate, sempre é necessário exportar o componente-->
 <script>
     export default{
         name: 'firstComponent',
+        data(){
+            return{
+                name:"Ovídio",
+                profession: "Dev Front-End"
+            }
+        }
     }
 </script>

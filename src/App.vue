9d037f8cb10136e0f19f7ea13a2f0fe3e972a960
Ovidiojcf <ojcfarias@gmail.com>
2025-02-18 16:49:20 -0300

    lifeCycleHooks implemented

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <firstComponent/>
+  <lifeCycle/>
 </template>
 <!-- Para utilizar algum componente nessa página é necessário importa-lo na tag script-->
 <script>
   import firstComponent from './components/firstComponent.vue';
+  import lifeCycle from './components/lifeCycle.vue';
   export default{
     name: 'App',
     components:{
-      firstComponent
+      firstComponent,
+      lifeCycle
     }
   }
 </script>
---
+++ src/components/lifeCycle.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1>My name is: {{ name }}</h1>
+</template>
+
+<script>
+    export default{
+        name: 'lyfeCyvel',
+        data(){
+            return{
+                name: 'Doesnt know'
+            }
+        },
+        created(){
+            setTimeout(()=>{
+                this.name = 'Ovidio'}, 1000
+            );
+        },
+        mounted(){
+            this.name = 'José'
+        }
+    }
+</script>

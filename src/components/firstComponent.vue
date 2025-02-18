99f14085682c59a38aa9496cc0ab146b22228735
Ovidiojcf <ojcfarias@gmail.com>
2025-02-18 15:54:04 -0300

    first component created

---
+++ src/components/firstComponent.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>Hello Ovídio! How is it going?</h1>
+</template>
+<!--Após a tag tamplate, sempre é necessário exportar o componente-->
+<script>
+    export default{
+        name: 'firstComponent',
+    }
+</script>

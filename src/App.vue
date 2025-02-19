88451788b86dfdb3a4206f35612052f63e096071
Ovidiojcf <ojcfarias@gmail.com>
2025-02-19 16:54:39 -0300

    add a HelloPerson with props

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,54 @@
 <template>
-  <firstComponent/>
-  <lifeCycle/>
-  <div>
-    <firstPerson/>
+  <div class="container">
+    <div style="margin: 10px;">
+      <input type="text" class="custom-input" v-model="name" placeholder="Type your name">
+    </div>
+    <div  style="margin: 5px;">
+      <!--Utiliza :variavel para conciliar a reatividade da variavel-->
+      <HelloPerson :name="name"/>
+    </div>
+    <div>
+      
+    </div>
   </div>
 </template>
 <!-- Para utilizar algum componente nessa página é necessário importa-lo na tag script-->
-<script>
-  import firstComponent from './components/firstComponent.vue';
-  import lifeCycle from './components/lifeCycle.vue';
-  import firstPerson from './components/firstPerson.vue';
+<script setup>
+import { ref } from 'vue';
+import HelloPerson from './components/HelloPerson.vue';
+const name = ref("");
 
-  export default{
-    name: 'App',
-    components:{
-      firstComponent,
-      lifeCycle,
-      firstPerson
-    }
-  }
 </script>
+
+<style>
+  .container{
+    display: column;
+    justify-content: space-between;
+    font-family:
+    Inter,
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    Oxygen,
+    Ubuntu,
+    Cantarell,
+    'Fira Sans',
+    'Droid Sans',
+    'Helvetica Neue',
+    sans-serif;
+  }
+  .custom-input{
+    padding: 10px;
+    border: 2px solid hsla(160, 100%, 37%, 1);
+    border-radius: 5px;
+    font-size: 40px;
+  }
+  .primary-button{
+    font-size: 20px;
+    background:  hsla(160, 100%, 37%, 0.2);
+    border: 2px solid;
+    border-radius: 5px;
+    
+  }
+</style>
---
+++ src/components/HelloPerson.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>Hello, {{name}}! Hope you're having a</h1>
+    <h1 class="text-bold-green">great day!</h1>
+</template>
+
+
+<script setup>
+defineProps({
+    name: String,
+});
+</script>
+
+<style>
+    .text-bold-green{
+        font-size: 70px;
+        color: hsla(160, 100%, 37%, 1);
+        font-weight: bold;
+        text-align: start;
+    }
+</style>
